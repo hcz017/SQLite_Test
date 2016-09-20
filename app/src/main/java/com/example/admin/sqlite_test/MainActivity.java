0cfcc6aba32a82d8923ccee76d8c34b33a92f7b2
chengzhi.hou <chengzhi.hou@ck-telecom.com>
2016-09-20 20:55:41 +0800

    query data

--- app/src/main/java/com/example/admin/sqlite_test/MainActivity.java
+++ app/src/main/java/com/example/admin/sqlite_test/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.admin.sqlite_test;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private MyDatabaseHelper dbHelper;
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +63,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 db.delete("Book", "pages > ?", new String[]{"500"});
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db =dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()){
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        String pages = cursor.getString(cursor.getColumnIndex("pages"));
+                        String price = cursor.getString(cursor.getColumnIndex("price"));
+                        Log.d(TAG, "book name is " + name);
+                        Log.d(TAG, "book author is " + author);
+                        Log.d(TAG, "book pages is " + pages);
+                        Log.d(TAG, "book price is " + price);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
